35e91b29c1181ea89d8e7f3f79aff5f19da773be
EmmiDev <emma@gmail.com>
2023-10-19 21:17:20 -0500

    initial commit

---
+++ accounts/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from accounts import views, users
+
+app_name = 'accounts'
+
+urlpatterns = [
+path('', views.index, name='index'),
+path('user-profile', users.user_profile, name='user_profile'),
+       ###########          ###########
+          ##     sign up urls  ##
+       ###########          ###########
+path('enroll-step1', views.enroll_step1, name='enroll_step1'),
+path('enroll-step2', views.enroll_step2, name='enroll_step2'),
+path('enroll-step3', views.enroll_step3, name='enroll_step3'),
+]
