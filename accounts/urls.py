5dc5df8e39dbe8d643e870933ae0bb8f4c4d29dd
EmmiDev <emma@gmail.com>
2023-10-23 20:19:53 -0500

    login page ready

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,7 +9,11 @@ path('user-profile', users.user_profile, name='user_profile'),
        ###########          ###########
           ##     sign up urls  ##
        ###########          ###########
+path('login', views.login, name='login'),
 path('enroll-step1', views.enroll_step1, name='enroll_step1'),
 path('enroll-step2', views.enroll_step2, name='enroll_step2'),
 path('enroll-step3', views.enroll_step3, name='enroll_step3'),
+path('enroll-step4', views.enroll_step4, name='enroll_step4'),
+path('enroll-step5', views.enroll_step5, name='enroll_step5'),
+path('enroll-complete', views.enroll_complete, name='enroll_complete'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -4,8 +4,11 @@ from .modules import *
 def index(request):
      return render(request, 'account_templates/index.html', {})
 
+def login(request):
+     return render(request, 'account_templates/login.html')
 
-    ##### Sign Up Views
+
+    ##### Sign Up Views  #######
 def enroll_step1(request):
      return render(request, 'account_templates/enroll_step1.html', {})
 
@@ -14,3 +17,12 @@ def enroll_step2(request):
 
 def enroll_step3(request):
      return render(request, 'account_templates/enroll_step3.html', {})
+
+def enroll_step4(request):
+     return render(request, 'account_templates/enroll_step4.html', {})
+
+def enroll_step5(request):
+     return render(request, 'account_templates/enroll_step5.html', {})
+
+def enroll_complete(request):
+     return render(request, 'account_templates/enroll_complete.html', {})
