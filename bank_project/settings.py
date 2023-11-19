e50d21cfafefddadb3d1ab0b097dadec4f5f5a5b
EmmiDev <emma@gmail.com>
2023-11-19 04:32:04 -0600

    session expiration

--- bank_project/settings.py
+++ bank_project/settings.py
@@ -44,7 +44,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "adminapp.middleware.UserActivityMiddleware" ### user broswer, ip and date
 ]
 
 ROOT_URLCONF = "bank_project.urls"
