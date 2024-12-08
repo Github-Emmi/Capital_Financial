eae874f8b19488c15a152e59541f27e2fc2716a4
EmmiDev <emma@gmail.com>
2024-12-09 05:26:44 +0700

    some changes

--- accounts/middleware.py
+++ accounts/middleware.py
@@ -1,7 +1,6 @@
 # middleware.py
-from django.shortcuts import redirect
+from django.shortcuts import render
 from django.contrib.auth import logout
-from django.urls import reverse
 
 
 class BlockedUserMiddleware:
@@ -10,6 +9,6 @@ class BlockedUserMiddleware:
 
     def __call__(self, request):
         if request.user.is_authenticated and request.user.is_blocked:
-            logout(request)
-            return redirect("account-blocked")
+            logout(request)  # Ensure the user is logged out
+            return render(request, "account_templates/account_blocked.html")
         return self.get_response(request)
