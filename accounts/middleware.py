1563fd1902c855a474c888078b01c7a459812547
EmmiDev <emma@gmail.com>
2024-12-09 05:33:25 +0700

    some changes

--- accounts/middleware.py
+++ accounts/middleware.py
@@ -2,13 +2,18 @@
 from django.shortcuts import render
 from django.contrib.auth import logout
 
-
 class BlockedUserMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
         if request.user.is_authenticated and request.user.is_blocked:
-            logout(request)  # Ensure the user is logged out
-            return render(request, "account_templates/account_blocked.html")
+            return render(request, "account_templates/account_blocked.html", {
+                "email": request.user.email,
+                "first_name": request.user.first_name,
+                "last_name": request.user.last_name,
+                "formatted_balance": request.user.formatted_balance,
+                "date_flagged": request.user.date_flagged,
+            })
         return self.get_response(request)
+
